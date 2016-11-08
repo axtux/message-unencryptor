6dd36683c5741d58277979353d0974a4ffc5c107
Axtux <axtux@hotmail.com>
2016-11-08 12:53:10 +0100

    Algorithm works !

--- code/DecodeLeMessagedAlice.m
+++ code/DecodeLeMessagedAlice.m
@@ -5,33 +5,15 @@
 clear all; clc; 
 
 % load la matrice d'encodage et le message d'Alice transmis par le canal: 
-load '\\umons.ac.be\student\121609\Desktop\Projet Opti\DecodeLeMessagedAlice\messagedAlice.mat'
+load 'messagedAlice.mat'
 
 % Utilisez votre algorithme pour résoudre:
 % 
 %   min_{0 <= xprime <= 1} ||A*xprime - yprime||_2^2 
 %
 % Rem. La solution xprime devrait etre un vecteur de taille 160x1
-A1 = A;
-[m, p] = size(A1);
 
-
-% Constraintes :
-A2 = [-1*eye(p) zeros(p, m) % Vi, -xi <= 0
-eye(p) zeros(p, m)          % Vi, x <= 1
-A1 -1*eye(m)                 % Vi, -ti + Ax <= yprime
--1*A1 -1*eye(m)              % Vi, -ti - Ax <= -yprime
-];
-b = [zeros(1, p) ones(1, p) transpose(yprime) -1*transpose(yprime)];
-
-f = [zeros(1, p) ones(1, m)];
-
-
-
-xprime = linprog(f, A2, b);
-
-
-%xprime = votrealgorithme(A, yprime);
+xprime = votrealgorithme(A, yprime);
 
 % Affichez le resultat: 
 fprintf('The recovered message is: %s \n', decoding_bin(xprime,d)); 
--- code/votrealgorithme.m
+++ code/votrealgorithme.m
@@ -1,19 +1,26 @@
-% Votre algorithme pour résoudre 
+% Votre algorithme pour resoudre 
 % 
 %   min_{0 <= xprime <= 1} ||A*xprime - yprime||_1 
 
 function xprime = votrealgorithme(A1, yprime) 
 
-[m,p] = size(A1);
-% Constraintes : Vi, -xi <= 0 && x <= 1
-A2 = [-1*eye(p)
-eye(p)];
-b = [zeros(1, p) ones(1, p)];
-
-f = [];
+[m, p] = size(A1);
 
 
+% Constraintes :
+A2 = [-1*eye(p) zeros(p, m) % Vi, -xi <= 0
+eye(p) zeros(p, m)          % Vi, x <= 1
+A1 -1*eye(m)                 % Vi, -ti + Ax <= yprime
+-1*A1 -1*eye(m)              % Vi, -ti - Ax <= -yprime
+];
+b = [zeros(1, p) ones(1, p) transpose(yprime) -1*transpose(yprime)];
 
-xprime = linprog(f, A2, b);
+C = [zeros(1, p) ones(1, m)];
+lb = zeros(1, p+m);
+ub = Inf(1, p+m);
+ctype = repmat("U", 1, 2*(p+m));
 
-xprime = zeros(size(A,2),1); % solution triviale
+% glpk (c, A, b, lb, ub, ctype, vartype, sense, param)
+xprime = glpk(C, A2, b, lb, ub, ctype);
+
+%xprime = zeros(size(A,2),1); % solution triviale
